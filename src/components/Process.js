d32d0c60206c09b7210a21a47b2b85e96424cff5
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-03-23 22:24:15 +0530

    services sectioin created, responsive

--- src/components/Process.js
+++ src/components/Process.js
@@ -7,11 +7,11 @@ export default function Process() {
         <picture className='hwe-image'>
         </picture>
         <div className="hwe-text">
-            <h1>We Belive in Lorem ipsum dolor sit Lorem, ipsum dolor.</h1>
-            <ul className='remove-list-style'>
-                <li>Lorem, ipsum dolor.</li>
-                <li>Lorem, ipsum dolor.</li>
-                <li style={{'marginBottom':'0'}}>Lorem, ipsum dolor.</li>
+            <h1>We Follow A Proper Process To Edit Videos Professionally</h1>
+            <ul >
+                <li>We first do this</li>
+                <li>Then we do this</li>
+                <li style={{'marginBottom':'0'}}>And finally we do this</li>
             </ul>
         </div>
     </section>
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,34 @@
-import React from 'react'
-import '../styles/services.css'
+import React from "react";
+import "../styles/services.css";
+import yt from "../images/youtube.png"
 
 export default function Services() {
   return (
     <section className="services">
-        <h1 className='center'>Our Services</h1>
+      <h1 className="center">Our Services</h1>
+      <p className="center" style={{'textAlign':'center'}}>We Specialize In Vlog And Gaming Videos Editing.</p>
+      <div className="service-cards">
+        <div className="service-card">
+          <img src={yt} alt="vlog like a pro image" />
+          <h3>Edit Vlogs</h3>
+          <p>service description Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="service-card">
+          <img src="yt" alt="" />
+          <h3>Game Like A Pro</h3>
+          <p>service description Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="service-card">
+          <img src="yt" alt="" />
+          <h3>Teach Like A Pro</h3>
+          <p>service description Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div className="service-card">
+          <img src="yt" alt="" />
+          <h3>Vlog Like A Pro</h3>
+          <p>service description Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
