11717500adead8d6eef12e058cbfa5d3391f4076
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-03-24 22:45:51 +0530

    responsive navbar created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,42 @@
-import React from 'react'
-import '../styles/navbar.css'
+import React, { useState, useRef } from "react";
+import "../styles/navbar.css";
 
 export default function Navbar() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // let button = document.getElementById("button");
+
+  let navbarMenu = useRef(null)
+  const button = useRef(null)
+
+  function menuClicked() {
+    if (menuOpen === false) {
+      button.current.classList.add("open");
+      setMenuOpen(true);
+      navbarMenu.current.style.transform = 'translateX(0)';
+    } else {
+      button.current.classList.remove("open");
+      setMenuOpen(false);
+      navbarMenu.current.style.transform = 'translateX(100%)';
+    }
+  }
+
   return (
-    <nav className='navbar'>
-        <h1 className="logo">NSquare</h1>
-        <ul className='remove-list-style'>
-            <li>Home</li>
-            <li>Services</li>
-            <li>About</li>
-            <li className='mr-0'>Reviews</li>
-        </ul>
+    <nav className="navbar">
+      <h1 className="logo">NSquare</h1>
+      <ul id="ls-tabs" className="remove-list-style">
+        <li>Home</li>
+        <li>Services</li>
+        <li>About</li>
+        <li className="mr-0">Reviews</li>
+      </ul>
+      <div onClick={menuClicked} ref={button} id="button">
+        <div id="bars"></div>
+      </div>
+      <div ref={navbarMenu} className="navbar-menu">
+        <h1>Hello Bitch</h1>
+      </div>
     </nav>
-  )
+  );
 }
