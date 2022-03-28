c474d9de87a3429ef8a35c1a0430def140410e35
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-03-28 19:03:47 +0530

    past work section created

--- src/components/PastWork.js
+++ src/components/PastWork.js
@@ -5,6 +5,13 @@ export default function PastWork() {
   return (
     <section className="pw">
         <h1 className='center'>Past Work</h1>
+        <div className="pw-section">
+          <div className="videos">
+        <iframe src="https://www.youtube.com/embed/jex5W6ou1Qc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/8GTf-0uo8Pw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/GLIEZaRIikM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        </div>
     </section>
   )
 }
