edf8a01ed10e6f7c6c413fad1b66f22e03a9751e
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-03-29 22:46:15 +0530

    testimonial section now functioning

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,34 +3,51 @@ import "../styles/testimonials.css";
 
 export default function Testimonials() {
 
-  let id = useRef('1')
+  let id = 1
   let img = useRef(null)
   let name = useRef(null)
   let taarif = useRef(null)
 
   function nextTestimonial() {
-    if (id === '1') {
+    if (id === 1) {
       name.current.innerHTML = 'John Blake'
       img.current.src = "https://randomuser.me/api/portraits/men/57.jpg"
       taarif.current.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo accusantium eius doloremque assumenda pariatur? Voluptatem consectetur iusto!"
-      id = '2'
+      id = 2
     }
-    if (id === '2') {
-      name.current.innerHTML = 'HE HE HE HE'
+    else if (id === 2) {
+      name.current.innerHTML = 'Parth Makwana'
       img.current.src = "https://randomuser.me/api/portraits/men/3.jpg"
-      taarif.current.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo accusantium eius doloremque assumenda pariatur? Voluptatem consectetur iusto fugiat ratione atque minus quos ipsam fuga maiores tenetur odit magni eius tempora distinctio, obcaecati praesentium vero!"
-      id = '3'
+      taarif.current.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo accusantium eius doloremque assumenda pariatur? Voluptatem consectetur iusto fugiat."
+      id = 3
     }
-    if (id === '3') {
+    else if (id === 3) {
       name.current.innerHTML = 'Utkarsh Singh'
       img.current.src = "https://avatars.githubusercontent.com/u/93197668?s=400&u=f18347d…&v=4"
       taarif.current.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at, inventore molestiae accusantium commodi veritatis."
-      id = '1'
+      id = 1
     }
-  } 
+  }
 
   function prevTestimonial() {
-    return null
+    if (id === 1) {
+      name.current.innerHTML = 'Parth Makwana'
+      img.current.src = "https://randomuser.me/api/portraits/men/3.jpg"
+      taarif.current.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo accusantium eius doloremque assumenda pariatur? Voluptatem consectetur iusto fugiat."
+      id = 3
+    }
+    else if (id === 2) {
+      name.current.innerHTML = 'Utkarsh Singh'
+      img.current.src = "https://avatars.githubusercontent.com/u/93197668?s=400&u=f18347d…&v=4"
+      taarif.current.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore at, inventore molestiae accusantium commodi veritatis."
+      id = 1
+    }
+    else if (id === 3) {
+      name.current.innerHTML = 'John Blake'
+      img.current.src = "https://randomuser.me/api/portraits/men/57.jpg"
+      taarif.current.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo accusantium eius doloremque assumenda pariatur? Voluptatem consectetur iusto!"
+      id = 2
+    }
   }
 
   return (
@@ -38,7 +55,7 @@ export default function Testimonials() {
       <h1 className="center">Testimonials</h1>
       <div className="testimonial-section center">
         <div className="testimonial" id="t-1">
-          <i className="fa-solid fa-chevron-left"></i>
+          <i onClick={()=>prevTestimonial()} className="fa-solid fa-chevron-left"></i>
           <div className="testimonial-content">
             <input value={1
             } type="hidden" className="id" />
